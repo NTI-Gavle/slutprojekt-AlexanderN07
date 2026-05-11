64a3126d65570b90b1868f2cd702117a16df95a1
AlexanderN07 <alexander.nurmi@elev.ga.ntig.se>
2026-05-11 14:43:00 +0200

    I've fixed the homepage(mostly)

---
+++ Web/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="website.js" defer></script>
+    <link rel="stylesheet" href="website.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <aside>
+        <h2>Following</h2>
+        <div>
+            <p>Placeholder for username</p>
+        </div>
+    </aside>
+    <nav>
+        <a href="chats.php">ph chat</a>
+        <a href="notifications.php">ph notif</a>
+        <a href="home.php">ph homebtn</a>
+        <a href="profile.php">ph profile</a>
+        <a href="settings.php">ph settings</a>
+    </nav>
+</body>
+</html>
---
+++ Web/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="website.js" defer></script>
+    <link rel="stylesheet" href="website.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
